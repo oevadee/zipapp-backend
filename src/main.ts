bc2ad420e75aaa7d619ac6693ed8042338ef6528
oveadee <a.szczechura.98@gmail.com>
2023-01-07 20:35:52 +0100

    feat: init users crud, refactor folder structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(3000);
 }
 bootstrap();
