35a1f42beedb618063df1c493db3e9e09118dfb1
oveadee <a.szczechura.98@gmail.com>
2023-01-21 21:11:35 +0100

    feat: init mongoose connection, add nvmrc, logger middleware

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { logger } from './middlewares/logger.middleware';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(logger);
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '../auth/auth.module';
 import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(`mongodb://docker:mongopw@localhost:55000`),
+    UsersModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
